66af1f3562079786398d50c35b3bba25ad91c5ea
void-jw <justinwu880520@gmail.com>
2020-08-23 16:03:09 +0800

    Add subplots to utilites/plot_reports_plotly.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ dataset = "minos"
 
 if dataset == "minos":
     path = "/run/user/1001/gvfs/sftp:host=140.114.27.95,port=50002/NFS/kike/minos/3dv2020"
-    scene = "pRbA3pwrgk9" + "/0"
+    scene = "1LXtFkjw3qL" + "/1"
     idx_frame = 230
 elif dataset == "tum_rgbd":
     path = "/run/user/1001/gvfs/sftp:host=140.114.27.95,port=50002/NFS/kike/TUM_RGBD/Testing_and_debugging/unzip/"
