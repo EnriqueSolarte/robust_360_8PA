80f4d516bf816b0159805aa9e753b4a05fb729c3
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2020-09-08 15:55:12 +0800

    minor changes

--- utilities/data_utilities.py
+++ utilities/data_utilities.py
@@ -14,18 +14,6 @@ COLOR_8PA = 'rgb(30,144,255)'
 COLOR_NORM_8PA = 'rgb(255,127,80)'
 
 
-def normalizer_s_k(x, s, k):
-    n_matrix = np.eye(3) * s
-    n_matrix[2, 2] = k
-    return n_matrix @ x, n_matrix
-
-
-def normalizer_s(x, s):
-    n_matrix = np.eye(3) * s
-    n_matrix[2, 2] = 10 / s
-    return n_matrix @ x, n_matrix
-
-
 def get_bearings(**kwargs):
     bearings_kf, bearings_frm, cam_gt, kwargs, ret = track_features(**kwargs)
     # ! 8PA Evaluation
@@ -143,50 +131,3 @@ def track_features(**kwargs):
     kwargs["idx_frame"] = kwargs["tracker"].frame_idx
 
     return bearings_kf, bearings_frm, relative_pose, kwargs, ret
-
-
-def get_frobenius_norm(x1, x2, return_A=False):
-    assert x1.shape == x2.shape
-    assert x1.shape[0] == 3
-
-    A = g8p.building_matrix_A(x1=x1, x2=x2)
-    c_fro_norm = np.linalg.norm(A.T.dot(A), ord="fro")
-    if return_A:
-        return c_fro_norm, A
-    return c_fro_norm
-
-
-def get_delta_bound_by_bearings(x1, x2):
-    assert x1.shape == x2.shape
-    assert x1.shape[0] == 3
-
-    n = x1.shape[1]
-    c_fro_norm = get_frobenius_norm(x1, x2) ** 2
-    # return c_fro_norm
-    # print(c_fro_norm)
-    sqr_a = (8 * c_fro_norm - n ** 2) / 7
-
-    sqr_b = (n / 8) - (1 / 8) * np.sqrt(sqr_a)
-    delta = np.sqrt(sqr_b)
-    return delta, c_fro_norm
-
-
-def get_delta_bound(observed_matrix):
-    """
-    Returns the evaluation of the delta - gap value, which
-    relates sensibility of a 8PA solution given the observed
-    matrix A (n,9). n>8 matched points.
-    """
-    assert observed_matrix.shape[1] == 9
-
-    # ! Equ (21) [Silveira CVPR 19']
-
-    n = observed_matrix.shape[0]
-    # ! c_fro_norm has  to be small
-    c_fro_norm = np.linalg.norm(observed_matrix.T.dot(observed_matrix),
-                                ord="fro") ** 2
-    # print(c_fro_norm)
-    sqr_a = (8 * c_fro_norm - n ** 2) / 7
-    sqr_b = (n / 8) - (1 / 8) * np.sqrt(sqr_a)
-    delta_bound = np.sqrt(sqr_b)
-    return delta_bound
