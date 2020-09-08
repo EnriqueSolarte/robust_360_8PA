6e79fa3f3e4f12ec7b034d2ae01ddcb1bb19580b
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2020-09-08 11:53:26 +0800

    minor changes

--- analysis/utilities.py
+++ analysis/utilities.py
@@ -8,6 +8,63 @@ from geometry_utilities import *
 from solvers.epipolar_constraint import EightPointAlgorithmGeneralGeometry as g8p
 import os
 from file_utilities import save_obj, load_obj
+from image_utilities import get_mask_map_by_res_loc
+
+COLOR_8PA = 'rgb(30,144,255)'
+COLOR_NORM_8PA = 'rgb(255,127,80)'
+
+
+def normalizer_s_k(x, s, k):
+    n_matrix = np.eye(3) * s
+    n_matrix[2, 2] = k
+    return n_matrix @ x, n_matrix
+
+
+def normalizer_s(x, s):
+    n_matrix = np.eye(3) * s
+    n_matrix[2, 2] = 10 / s
+    return n_matrix @ x, n_matrix
+
+
+def get_bearings(**kwargs):
+    bearings_kf, bearings_frm, cam_gt, kwargs, ret = track_features(**kwargs)
+    # ! 8PA Evaluation
+    kwargs["bearings"] = dict()
+
+    if kwargs.get("use_ransac", False):
+        # ! Solving by using RANSAC
+        ransac = RansacEssentialMatrix(**kwargs)
+        _ = ransac.solve(data=(
+            bearings_kf.copy().T,
+            bearings_frm.copy().T)
+        )
+        num_inliers = sum(ransac.current_inliers)
+        num_of_samples = len(ransac.current_inliers)
+        kwargs["bearings"]["rejections"] = 1 - (num_inliers / num_of_samples)
+        kwargs["bearings"]["ransac_residuals"] = ransac.current_residual
+        kwargs["bearings"]["kf"] = bearings_kf[:, ransac.current_inliers]
+        kwargs["bearings"]["frm"] = bearings_frm[:, ransac.current_inliers]
+    else:
+        kwargs["bearings"]["kf"] = bearings_kf
+        kwargs["bearings"]["frm"] = bearings_frm
+
+    kwargs["cam_gt"] = cam_gt
+    return kwargs
+
+
+def single_eval_cam_pose_error(_print=True, **kwargs):
+    cams = [cam for cam in kwargs.keys() if "cam" in cam and "gt" not in cam]
+    cam_gt = kwargs["cam_gt"]
+    for cam in cams:
+        cam_pose = kwargs[cam]
+        kwargs["error_" + cam] = evaluate_error_in_transformation(
+            transform_est=cam_pose,
+            transform_gt=cam_gt
+        )
+        if _print:
+            print("{} Error-rot: {}".format(cam, kwargs["error_" + cam][0]))
+            print("{} Error-tran: {}".format(cam, kwargs["error_" + cam][1]))
+    return kwargs
 
 
 def msk(eval, quantile):
@@ -21,6 +78,12 @@ def msk(eval, quantile):
 def save_results(**kwargs):
     filename = kwargs["filename"]
     dir_output = os.path.join("plots/{}.data".format(filename))
+    save_obj(dir_output, kwargs["results"])
+
+
+def save_surface_results(**kwargs):
+    filename = kwargs["filename"]
+    dir_output = os.path.join("plots/{}.data".format(filename))
 
     dt = dict(results=kwargs["results"],
               v_grid=kwargs["v_grid"],
@@ -29,8 +92,15 @@ def save_results(**kwargs):
 
 
 def track_features(**kwargs):
+    if not kwargs["tracker"].frame_idx + 1 < kwargs["data_scene"].number_frames:
+        return None, None, None, kwargs, False
+
+    kwargs["mask"] = get_mask_map_by_res_loc(kwargs["data_scene"].shape,
+                                             res=kwargs["res"],
+                                             loc=kwargs["loc"])
     initial_frame = kwargs["idx_frame"]
     idx = initial_frame
+    ret = True
 
     while True:
         frame_curr = Frame(**kwargs["data_scene"].get_frame(idx, return_dict=True),
@@ -70,7 +140,9 @@ def track_features(**kwargs):
     matches = kwargs["tracker"].get_matches()
     bearings_kf = cam.pixel2normalized_vector(matches[0])
     bearings_frm = cam.pixel2normalized_vector(matches[1])
-    return bearings_kf, bearings_frm, relative_pose, kwargs
+    kwargs["idx_frame"] = kwargs["tracker"].frame_idx
+
+    return bearings_kf, bearings_frm, relative_pose, kwargs, ret
 
 
 def get_frobenius_norm(x1, x2, return_A=False):
