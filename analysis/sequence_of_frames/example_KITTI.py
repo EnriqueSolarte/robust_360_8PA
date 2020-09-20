9457918fcffce7b0aff645028e1de62bf249ddba
void-jw <justinwu880520@gmail.com>
2020-09-20 17:49:11 +0800

    Fix error evaluation for KITTI dataset

--- analysis/sequence_of_frames/example_KITTI.py
+++ analysis/sequence_of_frames/example_KITTI.py
@@ -7,14 +7,14 @@ from structures.tracker import LKTracker
 error_n8p, error_8p = [], []
 
 
-def eval_camera_pose(tracker, cam_gt):
+def eval_camera_pose(cam, tracker, cam_gt):
     from solvers.epipolar_constraint import EightPointAlgorithmGeneralGeometry as g8p
     from solvers.optimal8pa import Optimal8PA as norm_8pa
 
     # g8p_norm = norm_8pa(version=opt_version)
     g8p = g8p()
 
-    cam = Pinhole(shape=tracker.initial_frame.shape)
+    # cam = Pinhole(shape=tracker.initial_frame.shape)
     matches = np.array(tracker.get_matches())
 
     kfrm = cam.pixel2normalized_vector(matches[0]).T
@@ -29,21 +29,25 @@ def eval_camera_pose(tracker, cam_gt):
     kwargs["iVal_Rpj_SK"] = (1, 1)
     kwargs["iVal_Res_RtSK"] = (1, 1)
     kwargs["bearings"] = dict()
-    kwargs["bearings"]["kf"] = kfrm
-    kwargs["bearings"]["frm"] = frm
+    kwargs["bearings"]["kf"] = Sphere.sphere_normalization(kfrm)
+    kwargs["bearings"]["frm"] = Sphere.sphere_normalization(frm)
 
     cam_opt_res_SK, _ = get_cam_pose_by_opt_res_error_SK(**kwargs)
     cam_opt_res_Rt, _ = get_cam_pose_by_opt_res_error_Rt(**kwargs)
     cam_opt_res_SK_Rt, _ = get_cam_pose_by_opt_res_error_SK_Rt(**kwargs)
 
-    print("8PA:         {}".format(evaluate_error_in_transformation(transform_gt=cam_gt,
+    print("8PA:             {}".format(
+        evaluate_error_in_transformation(transform_gt=cam_gt,
                                          transform_est=cam_8p)))
-    print("Opt_Res_SK:  {}".format(evaluate_error_in_transformation(transform_gt=cam_gt,
+    print("Opt_Res_SK:      {}".format(
+        evaluate_error_in_transformation(transform_gt=cam_gt,
                                          transform_est=cam_opt_res_SK)))
-    print("Opt_Res_Rt:  {}".format(evaluate_error_in_transformation(transform_gt=cam_gt,
-                                                                    transform_est=cam_opt_res_Rt)))
-    print("Opt_Res_SK_Rt:  {}".format(evaluate_error_in_transformation(transform_gt=cam_gt,
-                                                                    transform_est=cam_opt_res_SK_Rt)))
+    print("Opt_Res_Rt:      {}".format(
+        evaluate_error_in_transformation(transform_gt=cam_gt,
+                                         transform_est=cam_opt_res_Rt)))
+    print("Opt_Res_SK_Rt:   {}".format(
+        evaluate_error_in_transformation(transform_gt=cam_gt,
+                                         transform_est=cam_opt_res_SK_Rt)))
 
     print("kf:{} - frm:{} - matches:{}".format(tracker.initial_frame.idx,
                                                tracker.tracked_frame.idx,
@@ -66,8 +70,9 @@ if __name__ == "__main__":
     for idx in range(i, data.number_frames):
         frame_curr = Frame(**data.get_frame(idx, return_dict=True), idx=idx)
         if idx == i:
-            tracker.set_initial_frame(initial_frame=frame_curr,
-                                      extractor=Shi_Tomasi_Extractor(maxCorners=200))
+            tracker.set_initial_frame(
+                initial_frame=frame_curr,
+                extractor=Shi_Tomasi_Extractor(maxCorners=200))
             continue
 
         relative_pose = frame_curr.get_relative_pose(
@@ -75,10 +80,13 @@ if __name__ == "__main__":
         camera_distance = np.linalg.norm(relative_pose[0:3, 3])
 
         if camera_distance > threshold_camera_distance:
-            eval_camera_pose(tracker=tracker, cam_gt=relative_pose)
+            eval_camera_pose(cam=data.camera_projection,
+                             tracker=tracker,
+                             cam_gt=relative_pose)
             frame_prev = tracker.tracked_frame
-            tracker.set_initial_frame(initial_frame=frame_prev,
-                                      extractor=Shi_Tomasi_Extractor(maxCorners=200))
+            tracker.set_initial_frame(
+                initial_frame=frame_prev,
+                extractor=Shi_Tomasi_Extractor(maxCorners=200))
             relative_pose = frame_curr.get_relative_pose(
                 key_frame=tracker.initial_frame)
             camera_distance = np.linalg.norm(relative_pose[0:3, 3])
