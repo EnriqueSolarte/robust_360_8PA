f3223a6a736cf999a205429e46e4cdd4db20f67f
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2020-09-25 15:09:53 +0800

    minor changes

--- analysis/sequence_in_360_fov/all_solvers.py
+++ analysis/sequence_in_360_fov/all_solvers.py
@@ -12,8 +12,6 @@ def run_sequence(**kwargs):
     if "filename" not in kwargs.keys():
         kwargs["filename"] = get_file_name(**kwargs, file_src=__file__)
     print_log_files(kwargs["log_files"])
-    kwargs["results"] = dict()
-    kwargs["results"]["kf"] = []
     while True:
         kwargs, ret = get_bearings(**kwargs)
         if not ret:
@@ -23,7 +21,6 @@ def run_sequence(**kwargs):
         )
         print("{}".format(kwargs["filename"]))
         # ! Based on RESIDUALS
-        kwargs["results"]["kf"].append(kwargs["tracker"].initial_frame.idx)
         kwargs["cam_8pa"], kwargs["loss_8pa"] = get_cam_pose_by_8pa(**kwargs)
         kwargs["cam_OURS_opt_res_ks"], kwargs[
             "loss_OURS_RES_ks"] = get_cam_pose_by_opt_res_error_SK(**kwargs)
@@ -32,7 +29,7 @@ def run_sequence(**kwargs):
         # kwargs["cam_OURS_opt_res_Rtks"], kwargs["loss_OURS_RES_Rtks"] = get_cam_pose_by_opt_res_error_RtSK(**kwargs)
         kwargs["cam_OURS_opt_res_ks_Rt"], kwargs[
             "loss_OURS_RES_ks_Rt"] = get_cam_pose_by_opt_res_error_SK_Rt(
-                **kwargs)
+            **kwargs)
         # ! Based on REPROJECTION
         # kwargs["cam_PnP_opt_rpj_Rt"], kwargs["loss_PnP"] = get_cam_pose_by_opt_rpj_Rt_pnp(**kwargs)
         # kwargs["cam_OURS_opt_prj_sk"], kwargs["loss_OURS_RPJ_ks"] = get_cam_pose_by_opt_rpj_SK(**kwargs)
@@ -44,14 +41,15 @@ def run_sequence(**kwargs):
 if __name__ == '__main__':
     path = "/home/kike/Documents/datasets/MP3D_VO"
     # scene = "2azQ1b91cZZ/0"
+    scene = "pRbA3pwrgk9/0"
     # scene = "i5noydFURQK/0"
-    scene = "sT4fr6TAbpF/0"
+    # scene = "sT4fr6TAbpF/0"
     # scene = "1LXtFkjw3qL/0"
     # scene = "759xd9YjKW5/0"
-    basedir = "/home/justin/slam/openvslam_norm/python_scripts/synthetic_points_exp/data/3dv2020"
+    # basedir = "/home/justin/slam/openvslam_norm/python_scripts/synthetic_points_exp/data/3dv2020"
 
     # path = "/run/user/1001/gvfs/sftp:host=140.114.27.95,port=50002/NFS/kike/minos/vslab_MP3D_VO/512x1024"
-    data = MP3D_VO(scene=scene, basedir=basedir)
+    data = MP3D_VO(scene=scene, basedir=path)
     # from config import *
 
     # data = KITTI_VO(basedir="/home/justin/slam/datasets/KITTI/odometry",
@@ -66,19 +64,18 @@ if __name__ == '__main__':
         # res=(180, 180),
         # res=(65.5, 46.4),
         loc=(0, 0),
-        special_eval=True)
+        special_eval=False)
     initial_values = dict(
         iVal_Res_SK=(1, 1),
         iVal_Rpj_SK=(1, 1),
         iVal_Res_RtSK=(1, 1),
     )
     features_setting = dict(
-        feat_extractor=Shi_Tomasi_Extractor(maxCorners=1000),
+        feat_extractor=Shi_Tomasi_Extractor(maxCorners=500),
         tracker=LKTracker(),
         show_tracked_features=False,
-        sampling=10,
-        extra="sampling10",
-
+        # sampling=,
+        extra="09.25_normal",
     )
 
     ransac_parm = dict(
@@ -91,7 +88,7 @@ if __name__ == '__main__':
 
     log_settings = dict(
         log_files=(os.path.dirname(os.path.dirname(__file__)) +
-                   "/utilities/camera_recovering.py", ))
+                   "/utilities/camera_recovering.py",))
 
     kwargs = run_sequence(**scene_settings, **features_setting, **ransac_parm,
                           **initial_values, **log_settings)
